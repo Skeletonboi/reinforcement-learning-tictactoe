04d4f0c2a9dad9aff4351541f3127f1041991774
Victor Yip <victor.yip@mail.utoronto.ca>
2019-06-01 13:17:58 -0400

    changed to include neuron class

--- mdp.py
+++ mdp.py
@@ -1,16 +1,12 @@
 import numpy as np
-import torch
+#import torch
 import random
-from mrvic import neuron
+from neuron import neuron
 
 input = [0 for i in range(0,9)]
 
-hidden = [random.uniform(0,1) for i in range (0,5)]
+hidden = [neuron(input) for i in range(0,5)]
 
-output = [0 for i in range(0,2)]
+output = [neuron(hidden) for i in range(0,2)]
 
-for i in hidden:
-    wi = [random.uniform(0,1) for i in range (0,len(input))]
-    bi = random.uniform(0,1)
-    yi = np.dot(wi,input)
-    i = yi
+cost = yowhat
