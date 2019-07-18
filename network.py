a696090db2e94bba20e3b5df29064c2259e1e6d3
Victor Yip PC <victor.yip@mail.utoronto.ca>
2019-07-17 22:10:51 -0400

    Update network.py
    Revamping neuron class to network. Establishing MDP file as main file, added meta details on direction of project in network.py

--- network.py
+++ network.py
@@ -1,6 +1,19 @@
 from random import uniform
 class neuron:
-    def __init__(self,input):
-        self.wi = [uniform(0,1) for i in range(0,len(input))]
-        self.b = uniform(0,1)
-        
+    def __init__(self,input_length):
+        self.input_layer = [uniform(0,1) for i in range(0,input_length-1)]
+        self.hidden_layers = [
+
+
+#Q-Values for each action taken from a state estimated by neural network;
+# Network input are states;
+# Network output nodes are Q-values of each action taeken at the state;
+
+# Objective is to learn optimal Q-function (state-action valuation function)
+# that continues to satisfy Bellman optimality equation:
+#q*(s,a) = E[R_t+1 + discount*max_a'(q(s',a')]
+
+# NOTE: Stack of frames (in visual-input contexts) can be useful to gain more
+# information (ie. speed, direction);
+
+# Experiences
