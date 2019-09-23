5715fc01464be8d5bb67a29d048afbb71c748bfa
Victor Yip PC <victor.yip@mail.utoronto.ca>
2019-09-23 02:05:38 -0400

    Setting up multi-layer perceptron neural network for target and policy neural networks.

--- mdp.py
+++ mdp.py
@@ -1,14 +1,100 @@
 import torch
 import torch.nn as nn
+import torch.nn.Functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+import random
 
-class SNC(nn.Module):
+# Convert from numpy array to tensor
+
+# Experience Set
+
+# Initialize Hyperparameters
+seed = random.randint(1,1000)
+learningrate = 0.1
+numepoch = 50
+torch.manual_seed(seed)
+
+def accuracy(predictions,label):
+    total_corr = 0
+    index = 0
+    for c in predictions.flatten():
+        if (c.item() > 0.5):
+            r = 1.0
+        else:
+            r = 0.0
+        if (r == label[index].item()):
+            total_corr += 1
+        index +=1
+    return (total_corr/len(label))
+
+class MLP(nn.Module):
     def __init__(self):
-        super(SNC,self).__init__()
-        self.fc1 = nn.Linear(9,1)
+        super(MLP,self).__init__()
+        self.fc1 = nn.Linear(8,6)
+        self.fc2 = nn.Linear(6,4)
+        self.fc3 = nn.Linear(4,2)
 
-    def forward(self,I):
-        x = self.fc1(I)
+    def forward(self,x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
-smallNN = SNC()
-print(smallNN.fc1.weight)
+NN = MLP()
+
+print("Parameter Names and Initial (random) values: ")
+for name, param in MLP.named_parameters():
+    print("name:",name, "value:", param)
+
+predict = MLP(t_set)
+print('accuracy:',accuracy(predict,t_label))
+
+loss_function = nn.MSELoss()
+optimizer = torch.optim.SGD(MLP.parameters(),lr=learningrate)
+
+lossRec = []
+vlossRec = []
+nRec = []
+trainAccRec = []
+validAccRec = []
+for i in range(numepoch):
+    optimizer.zero_grad()
+    predict = MLP(t_set)
+    loss = loss_function(input = predict.squeeze(),target=t_label.float())
+    loss.backward()
+    optimizer.step()
+    trainAcc = accuracy(predict,t_label)
+    # Computing Validation accuracy and loss
+    predict = MLP(v_set)
+    vloss = loss_function(input=predict.squeeze(),target=v_label.float())
+    validAcc = accuracy(predict, v_label)
+
+    print("loss: ", f'{loss:.4f}', " trainAcc: ", f'{trainAcc:.4f}', " validAcc: ", f'{validAcc:.4f}')
+    lossRec.append(loss)
+    vlossRec.append(vloss)
+    nRec.append(i)
+    trainAccRec.append(trainAcc)
+    validAccRec.append(validAcc)
+
+# Plot out the loss and the accuracy, for both training and validation, vs. epoch
+
+plt.plot(nRec,lossRec, label='Train')
+plt.plot(nRec,vlossRec, label='Validation')
+plt.title('Training and Validation Loss vs. epoch')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+plt.plot(nRec,trainAccRec, label='Train')
+plt.plot(nRec,validAccRec, label='Validation')
+plt.title('Training and Validation Accuracy vs. epoch')
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.legend()
+plt.show()
+
+print("Model Weights")
+for name, param in MLP.named_parameters():
+    print("name:",name, "value:", param)
